8037b323aadd82c263344a12b9214fc350749cb9
Omar Nasr <omarnasr9975@gmail.com>
2026-08-10 00:01:02 +0300

    Delete Mohamed_Hany_Task_3 directory

--- Mohamed_Hany_Task_3/Tests/test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("final_internship_data.csv")
-print("Weather unique values:", df["Weather"].unique())
-print("Car Condition unique values:", df["Car Condition"].unique())
-print("Traffic Condition unique values:", df["Traffic Condition"].unique())
